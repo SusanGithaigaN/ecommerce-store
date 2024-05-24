6250b5dcc536a2baae912152886e1a8feeabd9a6
susan <susangithaiga26@gmail.com>
2024-05-24 03:13:41 +0300

    progress

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './pages/admin/login/login.component';
+import path from 'path';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    // set default route
+    {
+        path: '',
+        redirectTo: 'login',
+        pathMatch: 'full',
+    },
+    {
+        path: 'login',
+        component: LoginComponent
+    }
+
+];
